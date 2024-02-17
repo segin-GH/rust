567beeb14b97ad5a32fc143736af463f5dc2f7d4
segin-GH <segin.bytes@gmail.com>
2024-02-17 22:07:48 +0530

    example: more print fmts

--- helloWorld/hello.rs
+++ helloWorld/hello.rs
@@ -3,4 +3,33 @@
 fn main() {
     println!("Hello, world!");
     println!("I'm a Rustacean!");
+
+    println!("{} days", 31);
+
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+
+    println!("Base 10:               {}", 69420); // 69420
+    println!("Base 2 (binary):       {:b}", 69420); // 10000111100101100
+    println!("Base 8 (octal):        {:o}", 69420); // 207454
+    println!("Base 16 (hexadecimal): {:x}", 69420); // 10f2c
+    println!("Base 16 (hexadecimal): {:X}", 69420); // 10F2C
+
+    println!("{number:>9}", number = 1);
+
+    println!("{number:0>5}", number = 1); // 00001
+                                          // and left-adjust by flipping the sign. This will output "10000".
+    println!("{number:0<5}", number = 1); // 10000
+
+    println!("{number:0>width$}", number = 1, width = 5);
+
+    let number = 1;
+    let width: usize = 5;
+    println!("{number:>width$}");
 }
