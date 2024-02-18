1a40ad2ee4b725e831d1dd957e6de4922c266ecc
segin-GH <segin.bytes@gmail.com>
2024-02-18 21:32:14 +0530

    example: show datatypes

---
+++ data-types/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("Max i32: {}", std::i32::MAX);
+    println!("Min i32: {}", std::i32::MIN);
+    println!("Max i64: {}", std::i64::MAX);
+    println!("Min i64: {}", std::i64::MIN);
+    println!("Max i128: {}", std::i128::MAX);
+    println!("Min i128: {}", std::i128::MIN);
+    println!("Max f32: {}", std::f32::MAX);
+    println!("Min f32: {}", std::f32::MIN);
+    println!("Max f64: {}", std::f64::MAX);
+    println!("Min f64: {}", std::f64::MIN);
+    println!("Max u8: {}", std::u8::MAX);
+    println!("Min u8: {}", std::u8::MIN);
+    println!("Max u16: {}", std::u16::MAX);
+    println!("Min u16: {}", std::u16::MIN);
+    println!("Max u32: {}", std::u32::MAX);
+    println!("Min u32: {}", std::u32::MIN);
+    println!("Max u64: {}", std::u64::MAX);
+    println!("Min u64: {}", std::u64::MIN);
+    println!("Max u128: {}", std::u128::MAX);
+    println!("Min u128: {}", std::u128::MIN);
+    println!("Max isize: {}", std::isize::MAX);
+    println!("Min isize: {}", std::isize::MIN);
+    println!("Max usize: {}", std::usize::MAX);
+    println!("Min usize: {}", std::usize::MIN);
+
+}
